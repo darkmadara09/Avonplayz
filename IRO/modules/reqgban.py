c230f10698b2287f238d14e91d83fed7a49246f8
darkmadara09 <136217534+darkmadara09@users.noreply.github.com>
2024-02-22 12:47:38 +0530

    Update reqgban.py

--- IRO/modules/reqgban.py
+++ IRO/modules/reqgban.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 import os
+from IRO.modules.disable import DisableAbleCommandHandler
 from pyrogram import filters
 from pyrogram.types import (
     InlineKeyboardButton,

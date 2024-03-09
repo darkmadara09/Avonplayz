5216ce4c189fb01bd4e0f2e11dbf0b7c05b572ae
darkmadara09 <136217534+darkmadara09@users.noreply.github.com>
2024-03-09 15:55:57 +0530

    Update admin.py

--- IRO/modules/admin.py
+++ IRO/modules/admin.py
@@ -1038,7 +1038,7 @@ dispatcher.add_handler(DEMOTE_HANDLER)
 dispatcher.add_handler(SET_TITLE_HANDLER)
 dispatcher.add_handler(ADMIN_REFRESH_HANDLER)
 
-__mod_name__ = "ADMINS"
+__mod_name__ = "Aᴅᴍɪɴs"
 __command_list__ = [
     "setdesc" "setsticker" "setgpic" "delgpic" "setgtitle" "adminlist",
     "admins",

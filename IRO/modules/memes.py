53350b637f31f1949fc7c47a77dff8fb9bd9f49d
darkmadara09 <136217534+darkmadara09@users.noreply.github.com>
2024-03-14 14:22:44 +0530

    Update memes.py

--- IRO/modules/memes.py
+++ IRO/modules/memes.py
@@ -74,7 +74,9 @@ async def _(event):
     await stuber.edit("All News Has Been Sucessfully fetched, sendning to you."
                       )
     starknews = requests.get(main_url).json()
-    for item in starknews["data"]:
+    for item in starknews.get("data"):
+        if not item:
+            continue
         sedlyf = item["content"]
         img = item["imageUrl"]
         writter = item["author"]
@@ -82,7 +84,7 @@ async def _(event):
         readthis = item["readMoreUrl"]
         titles = item["title"]
         sed1 = img
-        sedm = f"**Title : {titles}** \n{sedlyf} \nDate : {dateis} \nAuthor : {writter} \nReadMore : {readthis}"
+        sedm = f"Title : {titles} \n{sedlyf} \nDate : {dateis} \nAuthor : {writter} \nReadMore : {readthis}"
         await pgram.send_photo(event.chat_id, sed1, caption=sedm)
 
 

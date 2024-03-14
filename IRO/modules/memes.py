c06e1afeac31ca0687bd088d4119a1cc380c0edd
darkmadara09 <136217534+darkmadara09@users.noreply.github.com>
2024-03-14 16:39:48 +0530

    Update memes.py

--- IRO/modules/memes.py
+++ IRO/modules/memes.py
@@ -67,7 +67,7 @@ async def _(event):
     if event.fwd_from:
         return
     infintyvar = event.pattern_match.group(1)
-    main_url = f"https://sugoi-api.vercel.app/news?keyword={infinityvar}"
+    main_url = f"https://sugoi-api.vercel.app/news?keyword={infintyvar}"
     stuber = await event.reply(
         f"Ok ! Fectching {infintyvar} From inshortsapi Server And Sending To News Channel",
     )

72c047a34dcc4e835e7461a9c69676ad6b76d1e1
darkmadara09 <136217534+darkmadara09@users.noreply.github.com>
2024-03-16 17:32:21 +0530

    Update barcode.py

--- IRO/modules/barcode.py
+++ IRO/modules/barcode.py
@@ -1,32 +1,40 @@
-from pyrogram import filters
-from pyrogram.types import  Message
-from pyrogram.enums import ChatAction
+from pyrogram import Client, filters
+from pyrogram.types import Message
+import qrcode
+from IRO import pbot
+from PIL import Image
+import io
 
-from .. import pbot as  IRO,BOT_USERNAME
-import requests
 
 
-@IRO.on_message(filters.command("qrcode"))
-async def qrcode_(_, message: Message):
-    if message.reply_to_message:
-        text = message.reply_to_message.text
+# Function to create a QR code
+def generate_qr_code(text):
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(text)
+    qr.make(fit=True)
+
+    img = qr.make_image(fill_color="white", back_color="black")
+
+    # Save the QR code to a bytes object to send with Pyrogram
+    img_bytes = io.BytesIO()
+    img.save(img_bytes, format='PNG')
+    img_bytes.seek(0)  # Go to the start of the bytes object
+
+    return img_bytes
+
+
+@pbot.on_message(filters.command("qr"))
+def qr_handler(client, message: Message):
+    # Extracting the text passed after the command
+    command_text = message.command
+    if len(command_text) > 1:
+        input_text = " ".join(command_text[1:])
+        qr_image = generate_qr_code(input_text)
+        message.reply_photo(qr_image, caption="Here's your QR Code")
     else:
-        text =message.text.split(None, 1)[1]
-    m =await message.reply_text( "» `ᴘʟᴇᴀsᴇ ᴡᴀɪᴛ...,\n\n❍ ᴄʀᴇᴀᴛɪɴɢ ʏᴏᴜʀ ǫʀᴄᴏᴅᴇ ...`")
-    write = requests.get(f"https://iro-api.vercel.app/qrcode/{text}").json()["results"]
-
-    caption = f"""
-sᴜᴄᴇssғᴜʟʟʏ ɢᴇɴᴇʀᴀᴛᴇᴅ ǫʀᴄᴏᴅᴇ 
-
-» **ɢᴇɴᴇʀᴀᴛᴇᴅ ʙʏ ➛** @{BOT_USERNAME}
-» **ʀᴇǫᴜᴇsᴛᴇᴅ ʙʏ ➛** {message.from_user.mention}
-"""
-    await m.delete()
-    await message.reply_photo(photo=write,caption=caption)
-# -----------CREDITS -----------
-# telegram : @legend_coder
-# github : noob-mukesh
-__mod_name__ = "ǫʀᴄᴏᴅᴇ"
-__help__ = """
- ❍ /qrcode ➛ ɢɪᴠᴇ sᴏᴍᴇ ᴛᴇxᴛ ᴛᴏ ɢᴇɴᴇʀᴀᴛᴇ ǫʀᴄᴏᴅᴇ
- """
+        message.reply_text("Please provide the text for the QR code after the command. Example usage: /qr text")

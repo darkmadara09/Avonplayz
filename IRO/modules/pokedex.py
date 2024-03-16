b32212e7e7b65cbdc388299632951f8c807e8475
darkmadara09 <136217534+darkmadara09@users.noreply.github.com>
2024-03-16 15:00:21 +0530

    Update pokedex.py

--- IRO/modules/pokedex.py
+++ IRO/modules/pokedex.py
@@ -45,7 +45,7 @@ async def PokeDex(_, message):
         await message.reply_text("/pokedex Pokemon Name")
         return
     pokemon = message.text.split(None, 1)[1]
-    pokedex = f'https://some-random-api.ml/pokedex?pokemon={pokemon}'
+    pokedex = f"https://sugoi-api.vercel.app/pokedex?pokemon={pokemon}"
     async with aiohttp.ClientSession() as session:
         async with session.get(pokedex) as request:
             if request.status == 404:

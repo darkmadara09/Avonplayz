df31f5ab4c918de5d862201ea27429c00e8e8b59
darkmadara09 <136217534+darkmadara09@users.noreply.github.com>
2024-03-22 14:33:50 +0530

    Update morning.py

--- IRO/modules/morning.py
+++ IRO/modules/morning.py
@@ -33,10 +33,10 @@ PHOTO = "https://telegra.ph/file/1b57ea5abf2f600370b01.mp4"
 
 @register(pattern=("Good morning"))
 async def awake(event):
-    NEKO = f"❀ ᴡɪsʜɪɴɢ ʏᴏᴜ ᴀ ᴠᴇʀʏ ɢᴏᴏᴅ ᴍᴏʀɴɪɴɢ ❀\n\n✦ ᴡᴇʟᴄᴏᴍᴇ ᴛʜɪs ʙᴇᴀᴜᴛɪғᴜʟ ᴍᴏʀɴɪɴɢ ᴡɪᴛʜ ᴀ sᴍɪʟᴇ ᴏɴ ʏᴏᴜʀ ғᴀᴄᴇ. I ʜᴏᴘᴇ ʏᴏᴜ ʟʟ ʜᴀᴠᴇ ᴀ ɢʀᴇᴀᴛ ᴅᴀʏ ᴛᴏᴅᴀʏ.\n\n✦ ᴡɪsʜɪɴɢ ᴛᴏ ➛ {event.sender.first_name}\n\n✦ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➛ [𝗗𝗔𝗭𝗔𝗜](https://t.me/dazai_ixbot)"
+    NEKO = f"❀ ᴡɪsʜɪɴɢ ʏᴏᴜ ᴀ ᴠᴇʀʏ ɢᴏᴏᴅ ᴍᴏʀɴɪɴɢ ❀\n\n✦ ᴡᴇʟᴄᴏᴍᴇ ᴛʜɪs ʙᴇᴀᴜᴛɪғᴜʟ ᴍᴏʀɴɪɴɢ ᴡɪᴛʜ ᴀ sᴍɪʟᴇ ᴏɴ ʏᴏᴜʀ ғᴀᴄᴇ. I ʜᴏᴘᴇ ʏᴏᴜ ʟʟ ʜᴀᴠᴇ ᴀ ɢʀᴇᴀᴛ ᴅᴀʏ ᴛᴏᴅᴀʏ.\n\n✦ ᴡɪsʜɪɴɢ ᴛᴏ ➛ {event.sender.first_name}\n\n✦ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➛ [𓆩『 ᴍᴜɪᴄʜɪʀᴏ ᴛᴏᴋɪᴛᴏ 』𓆪](https://t.me/Muichiro_xbot)"
     BUTTON = [
         [
-            Button.url("sᴜᴘᴘᴏʀᴛ", "https://telegram.dog/ix_updates"),
+            Button.url("sᴜᴘᴘᴏʀᴛ", "https://telegram.dog/Muichiro_support"),
         ]
     ]
     await telethn.send_file(event.chat_id, PHOTO, caption=NEKO, buttons=BUTTON)

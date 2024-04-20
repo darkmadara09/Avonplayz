55c878e809f1bb1d02048a1b40325ef4ba286f1c
darkmadara09 <136217534+darkmadara09@users.noreply.github.com>
2024-04-21 01:42:33 +0530

    Update spam.py

--- IRO/modules/spam.py
+++ IRO/modules/spam.py
@@ -31,7 +31,7 @@ from datetime import timedelta
 from telethon import events
 
 from IRO import pbot
-from IRO.modules.helper_funcs.chat_status import *
+from IRO.modules.helper_funcs.chat_status import is_admin
 
 
 @pbot.on(events.NewMessage(pattern="[!?/]spam"))

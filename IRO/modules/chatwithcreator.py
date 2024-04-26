65bdb07fbe8d90bdb005739844c0d2d0f1a54f2a
darkmadara09 <136217534+darkmadara09@users.noreply.github.com>
2024-04-26 22:06:11 +0530

    Update chatwithcreator.py

--- IRO/modules/chatwithcreator.py
+++ IRO/modules/chatwithcreator.py
@@ -1,20 +1,20 @@
 # change clients here
-from repo import client as app
+from IRO import client as pbot
 from pyrogram import filters
 from pyrogram.types import ForceReply, InlineKeyboardMarkup, InlineKeyboardButton
 
-@app.on_message(filters.command("contact"))
+@pbot.on_message(filters.command("contact"))
 async def contact(_, message):
     keyboard = InlineKeyboardMarkup([[InlineKeyboardButton("Chat With Creator", callback_data="live_chat")]])
     await message.reply_text("Click this button to contact my creator", reply_markup=keyboard)
 
-@app.on_callback_query(filters.regex("live_chat"))
+@pnot.on_callback_query(filters.regex("live_chat"))
 async def live_chat(_, query):
     await query.message.delete()
     string = """TO CONTACT MY CREATOR, JUST REPLY TO THIS MESSAGE"""
     await query.message.reply_text(text=string, reply_markup=ForceReply(True))
 
-@app.on_message(filters.reply & filters.private)
+@pbot.on_message(filters.reply & filters.private)
 async def live_chat_forward(_, message):
     reply = message.reply_to_message
     if reply and reply.reply_markup and isinstance(reply.reply_markup, ForceReply):
